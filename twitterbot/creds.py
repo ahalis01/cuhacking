5b37230e283a690c0552a3781faa6c04d59c8228
Connor Ciavarella <30648182+greenappleee@users.noreply.github.com>
2020-01-11 22:37:07 -0500

    Add files via upload

---
+++ twitterbot/creds.py
@@ -0,0 +1,7 @@
+# creds !!!.gitignore!!!
+
+creds = ["rImKeXZDJZhaFFICxHowvHx8H",
+        "TfS1y4Vvp0XBesZQtTRK5RAR2WRYgWaBbZ0q7EWqAs3oKoR1eU",
+        "1371244872-Nu7thZJS7WkoODuSEZbitno7BI0bOhej3ruiW2q",
+        "6E9bSTnXDwaHMw6yEQCLkOylqWKLTJuBFC2Py0OPHhFQY"
+        ]
